a5183354cd3832860a49cfe504da2686dd68f6d1
Andreas Cardeneo <andreas@cardeneo.de>
2025-01-06 14:01:47 +0100

    digits array and lambda function for aggMult

--- pe.cpp
+++ pe.cpp
@@ -155,32 +155,36 @@ constexpr u64 Problem8_LargestProductInASeries()
 {
     constexpr std::string_view numString{"7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"};
     constexpr size_t windowWidth{ 13u };
-    
+   
     if (numString.size() < windowWidth)
         return 0ull;
+    std::array<int, numString.size()> digits;
+    std::transform(begin(numString), end(numString), begin(digits), [](const char c) { return c - '0';});
+
     size_t startIdx{ 0u };
-    u64 max{ std::accumulate(begin(numString) + startIdx, begin(numString) + startIdx + windowWidth, 1ull, [](u64 agg, const char c) { return agg * (c - '0');}) };
-    std::string_view argMax{numString.substr(startIdx, windowWidth)};
+    auto aggMult = [](u64 agg, int d) -> u64 { return agg * d;};
+    u64 max{ std::accumulate(begin(digits), begin(digits) + windowWidth, 1ull, aggMult ) };
+    std::string_view argMax{numString.substr(0u, windowWidth)};
     u64 prod{ max };
     startIdx++;
     while(startIdx + windowWidth < numString.size())
     {   
-        while (numString[startIdx] == '0' && startIdx + windowWidth < numString.size())
+        while (digits[startIdx] == 0 && startIdx + windowWidth < numString.size())
             startIdx++;
         if (startIdx + windowWidth < numString.size())    
         {
-            prod = numString[startIdx] - '0';
+            prod = digits[startIdx];
             size_t idx{ 1u };
             for (; idx < windowWidth; ++idx)
             {
-                const char c = numString[startIdx + idx];
-                if (c == '0')
+                const int val = digits[startIdx + idx];
+                if (val == 0)
                 {
                     startIdx += idx;
                     break;
                 }
                 else
-                    prod *= c - '0';
+                    prod *= val;
             }
             if (idx == windowWidth && prod > max)
             {
